49fc52b428662d4f36e5c0cb80a77da49c7775ec
Leonel Godoy <godoyutn@gmail.com>
2021-01-04 20:08:24 +0100

    V37, Obteniendo registros de la BBDD, funciones mysqli_connect_errno(). mysqli_select_db(), mysqli_set_charset(), mysqli_close()

--- conexionBBDD.php
+++ conexionBBDD.php
@@ -6,23 +6,41 @@
 </head>
 <body>
 <?php 
+    //ubicacion de la BBDD, en este caso es LOCAL
     $db_host="localhost";
+    
+    //nombre de la BBDD- Aun no se la extension del archivo 
     $db_namebbdd="pruebas";
     $db_user="root";
     $db_password="";
-    
-    $conexion=mysqli_connect($db_host, $db_user, $db_password, $db_namebbdd);
     //guardo mi consulta en una vvariable
+    $conexion=mysqli_connect($db_host, $db_user, $db_password);
+    
+    
+    if(mysqli_connect_errno()){
+        echo "Error al conectar con la BBDD " . "<br>";
+        exit();
+    }
+    
+    mysqli_select_db($conexion, $db_namebbdd) or die("No se encuentra la BBDD");
+    
+    //instruccion para reconocer caracteres latinos
+    mysqli_set_charset($conexion, "utf8");
+    
     $query="SELECT * FROM DATOSPERSONALES";
     //luego, ejecuto mi consulta
     $resultados=mysqli_query($conexion, $query);
     
-    $fila = mysqli_fetch_row($resultados);
+        
+    while(($fila=mysqli_fetch_row($resultados))==TRUE){
+        echo $fila[0]. "  ";
+        echo $fila[1]. "  ";
+        echo $fila[2]. "  " . "<br>";
+    }
     
-    echo $fila[0]. "  ";
-    echo $fila[1]. "  ";
-    echo $fila[2]. "  " . "<br>";
     
+    //-------------cierro la conexion
+    mysqli_close($conexion);
 
 ?>
 </body>
