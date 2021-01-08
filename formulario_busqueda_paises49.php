f3fa2c29be29130acf8f76f65a4e90d47667c352
Leonel Godoy <godoyutn@gmail.com>
2021-01-08 17:00:58 +0100

    V49 Consultas preparadas para evitar inyeccion SQL. Archivo formulario_busqueda_paises49.php

---
+++ formulario_busqueda_paises49.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>V49, evitando inyeccion SQL con consultas preparadas</title>
+</head>
+
+<body>
+
+<form action="resultado_paises49.php" method="get">
+	<label>Introduce pais: <input type="text" name="buscar"></label>
+	<input type="submit" name="enviando" value="Go!">	
+</form>
+
+</body>
+</html>
